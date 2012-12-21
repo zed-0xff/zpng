b916589efbf1151c964f30a1dccd0dd9f2b4816b
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2012-12-21 21:06:28 +0400

    PNG test-suite fetching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,6 @@ require 'zpng'
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-RSpec.configure do |config|
-end
-
 SAMPLES_DIR = File.join(
   File.dirname(
     File.dirname(
@@ -22,3 +19,32 @@ SAMPLES =
   else
     Dir[File.join(SAMPLES_DIR,'qr_*.png')]
   end
+
+PNG_SUITE_URL = "http://www.schaik.com/pngsuite/PngSuite-2011apr25.tgz"
+
+def get_png_suite
+  dir = File.join(SAMPLES_DIR, "png_suite")
+  if Dir.exist?(dir)
+    if Dir[File.join(dir, "*.png")].size > 100
+      # already fetched and unpacked
+      return
+    end
+  else
+    Dir.mkdir(dir)
+  end
+  require 'open-uri'
+  puts "[.] fetching PNG test-suite from #{PNG_SUITE_URL} .. "
+  data = open(PNG_SUITE_URL).read
+
+  fname = File.join(dir, "png_suite.tgz")
+  File.open(fname, "wb"){ |f| f<<data }
+  puts "[.] unpacking .. "
+  system "tar", "xzf", fname, "-C", dir
+end
+
+RSpec.configure do |config|
+  config.before :suite do
+    get_png_suite
+  end
+end
+
